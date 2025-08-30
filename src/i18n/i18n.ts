d9ff2898dcabc82c695d8654d099f7f866c4b932
nightcoffee <nightcoffee@users.noreply.github.com>
2025-08-31 02:26:40 +0800

    Update i18n.ts

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -43,34 +43,12 @@ interface StringMap {
 
 const antdLocales: ObjectMap = {
     'en_US': antd_en_US,
-    'ja_JP': antd_ja_JP,
-    'zh_TW': antd_zh_TW,
-    'zh_CN': antd_zh_CN,
-    'de_DE': antd_de_DE,
-    'fr_FR': antd_fr_FR,
-    'es_ES': antd_es_ES,
-    'ko_KR': antd_ko_KR,
-    'pt_BR': antd_pt_BR,
-    'th_TH': antd_th_TH,
-    'el_GR': antd_el_GR,
-    'hi_IN': antd_hi_IN,
-    'vi_VN': antd_vi_VN
+    'zh_CN': antd_zh_CN
 }
 
 const dayJsLocales: StringMap = {
     'en_US': 'en',
-    'ja_JP': 'ja',
-    'zh_TW': 'zh-tw',
-    'zh_CN': 'zh-cn',
-    'de_DE': 'de-de',
-    'fr_FR': 'fr-fr',
-    'es_ES': 'es-es',
-    'ko_KR': 'ko',
-    'pt_BR': 'pt-br',
-    'th_TH': 'th',
-    'el_GR': 'el',
-    'hi_IN': 'hi',
-    'vi_VN': 'vi'
+    'zh_CN': 'zh-cn'
 }
 
 const locale: Ref<SupportedLocale> = ref('en_US');
@@ -86,36 +64,14 @@ const setLocale = async (localeString: SupportedLocale): Promise<Locale> => {
 const getLocaleName = (locale: SupportedLocale) => {
     switch (locale) {
         case 'en_US': default: return 'English'
-        case 'fr_FR': return 'Français (French)'
-        case 'de_DE': return 'Deutsch (German)'
-        case 'es_ES': return 'Español (Spanish)'
-        case 'ja_JP': return '日本語 (Japanese)'
-        case 'zh_TW': return '繁體中文 (Traditional Chinese)'
         case 'zh_CN': return '简体中文 (Simplified Chinese)'
-        case 'ko_KR': return '한국어 (Korean)'
-        case 'pt_BR': return 'Português (Brasil)'
-        case 'th_TH': return 'ไทย (Thai)'
-        case 'el_GR': return 'Ελληνικά (Greek)'
-        case 'hi_IN': return 'हिन्दी (Hindi)'
-        case 'vi_VN': return 'Tiếng Việt (Vietnamese)'
     }
 }
 
 const getLocaleCodeAlias = (locale: SupportedLocale) => {
     switch (locale) {
         case 'en_US': default: return 'gb'
-        case 'ja_JP': return 'jp'
-        case 'zh_TW': return 'hk'
         case 'zh_CN': return 'sg'
-        case 'de_DE': return 'de'
-        case 'fr_FR': return 'fr'
-        case 'es_ES': return 'es'
-        case 'ko_KR': return 'kr'
-        case 'pt_BR': return 'br'
-        case 'th_TH': return 'th'
-        case 'el_GR': return 'gr'
-        case 'hi_IN': return 'in'
-        case 'vi_VN': return 'vn'
     }
 }
 
@@ -126,4 +82,5 @@ export {
     SupportedLocales,
     getLocaleCodeAlias
 }
+
 
